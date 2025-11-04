2dbb0c7f5da82d476cf0002f89d83282c304c3a6
wonbin <wonbin3977@gmail.com>
2025-11-04 16:06:21 +0900

    refactor(db.js) : db.js 위치 configuration 으로 이동

--- express/index.js
+++ express/index.js
@@ -9,7 +9,7 @@ const app = express();
 applyCors(app); //cors 설정 적용
 app.use(express.json());  // JSON body 파싱
 
-app.use(authRoutes);
+app.use(authRoutes); // 인증 라우트
 
 app.get('/', (req, res) => {
   res.send('8080 서버 응답');
