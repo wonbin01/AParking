7839736ab381326a2c4a777c51654af6796a802e
wonbin <wonbin3977@gmail.com>
2025-12-03 15:33:26 +0900

    fix : vercel cors 수정

--- express/configuration/corsConfig.js
+++ express/configuration/corsConfig.js
@@ -3,7 +3,8 @@ import cors from "cors";
 export const corsOptions = {
   origin: [
     "http://localhost:5173",       
-    "https://a-parking.vercel.app"
+    "https://a-parking.vercel.app",
+    "https://a-parking-4fufdqnmz-wonbin01s-projects.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
 };
